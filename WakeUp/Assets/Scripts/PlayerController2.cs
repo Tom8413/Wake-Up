c63c7ebdfabaeec43bd4a7aac73a4081d4ba6365
Tomasz <tomaszszubarczyk@gmail.com>
2019-12-08 20:16:11 +0100

    Commit 9
    Jump

--- WakeUp/Assets/Scripts/PlayerController2.cs
+++ WakeUp/Assets/Scripts/PlayerController2.cs
@@ -13,6 +13,14 @@ public class PlayerController2 : MonoBehaviour
     bool facingRight;
     Vector3 localScale;
 
+    bool grounded = false;
+    //Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
+
     private void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
@@ -20,15 +28,47 @@ public class PlayerController2 : MonoBehaviour
         localScale = transform.localScale;
         facingRight = true;
     }
+    private void Update()
+    {
+         if( grounded && Input.GetAxis("Jump")>0)
+         {
+         grounded = false;
+         myAnim.SetBool("grounded", grounded);
+         myRB.AddForce(new Vector2(0, jumpHeight));
+         }
+    }
     private void FixedUpdate()
     {
+        //W Collision [] będzie informacja czy sfera pod nogami (o promieniu 0.2f) styka sie z podłożem. Wpierw ustawia sferę w groundCheck.position
+        // potem rysuje ją do rozmiarów 0.2f a następnie sprawdza czy ta kulka dotyka Layer grouded
+       // if( grounded && Input.GetAxis("Jump")>0)
+        //{
+         //   grounded = false;
+          //  myAnim.SetBool("grounded", grounded);
+           // myRB.AddForce(new Vector3(0, jumpHeight, 0));
+        //}
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        //if (groundCollisions.Length > 0)
+          //  grounded = true;
+        //else
+        
+        //    grounded = false;
+
+
+
+        myAnim.SetBool("grounded", grounded);
+        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+        
+  
         float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("Speed", Mathf.Abs(move));
 
         float walking = Input.GetAxisRaw("Fire3");
         myAnim.SetFloat("isWalking", walking);
 
-        if (walking >0)
+        if (walking >0 && grounded)
         {
             myRB.velocity = new Vector3(move * walkSpeed, myRB.velocity.y, 0);
         } else
@@ -42,8 +82,6 @@ public class PlayerController2 : MonoBehaviour
             facingRight = true;
             Flip();
         }
-
-            
 
         else if (move < 0)
         {
