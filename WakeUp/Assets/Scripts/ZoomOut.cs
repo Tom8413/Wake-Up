ef63fd484ed2b22a60810beed2415931e0621894
Tomasz <tomaszszubarczyk@gmail.com>
2020-01-07 19:27:24 +0100

    Commit 14
    Add change camera (cinemachine), boar + chain

---
+++ WakeUp/Assets/Scripts/ZoomOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour
+{
+    public string parametr;
+
+    public Animator camAnim;
+
+    public BoxCollider2D trigger;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider == trigger)
+        {
+            camAnim.SetBool(parametr, true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider == trigger)
+        {
+            camAnim.SetBool(parametr, false);
+        }
+    }
+}
